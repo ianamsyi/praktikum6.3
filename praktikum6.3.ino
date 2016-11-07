1c01033bee7763b26d55531640dd47359987619f
ianamsyi <ianamsyi19@gmail.com>
2016-11-07 16:28:43 +0700

    Add files via upload

---
+++ praktikum6.3.ino
@@ -0,0 +1,18 @@
+void setup() {
+TCCR1A=0x00;
+TCCR1B=0x06;
+TCNT1H=0x00;
+TCNT1L=0x00;
+ICR1H=0x00;
+ICR1L=0x00;
+OCR1AH=0x00;
+OCR1AL=0x00;
+OCR1BH=0x00;
+OCR1BL=0x00;
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
